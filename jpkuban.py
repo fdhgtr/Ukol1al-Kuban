81572e69bae1cfdb99aab8970cea012bef299d5b
Tobias <159123644+fdhgtr@users.noreply.github.com>
2024-11-06 09:23:31 +0000

    final_kuban

--- jpkuban.py
+++ jpkuban.py
@@ -1,1 +1,15 @@
-pole = ["", "", "", "", ""]
+pole = ["oheň", "voda", "teplo", "zima", "sníh"] #vytvoření pole s 5 stringovými hodnotami
+print(pole)#výpis kodu
+
+pole.append("vítr")#přidání jednoho prvku pomocí append
+print(pole)#výpis kodu
+
+pole.remove("voda")#odstranění druhého prvku pomocí remove
+print(pole)#výpis kodu
+
+pole[4] = "slunce"#změna páté hodnoty na slunce
+print(pole)#výpis kodu
+
+pole.extend(["podzim", "jaro"])#přidání dvou dalších stringových hodnot pomocí extend
+print(pole)#výpis kodu
+
